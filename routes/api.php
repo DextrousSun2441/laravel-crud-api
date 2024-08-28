95c27cb53f47aa81abcd105f17c631a0772e4b2b
DextrousSun2441 <116996197+DextrousSun2441@users.noreply.github.com>
2024-08-27 23:51:13 -0600

    Creando las funcionalidades publicas de la api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\studentController;
 
 /*Definicion de rutas: Route define rutas, cada ruta tiene un verbo HTTP Asociado */
 
@@ -12,22 +13,21 @@ Route::get('/students', function (){
 /*1° GET /Students:Esta ruta responde a las solicitudes 'GET' a la URL '/students. La funcion de devolución('callback')
 devuelve de la cadena obteniendo lista de estudiantes */
 
-Route::get('/students/{id}', function(){
-    return 'obteniendo un estudiante';
-});
+Route::get('/students/{id}', [studentController::class, 'show']);
 /*2° GET /students/{id}:Esta ruta responde a solicitudes 'GET' a la URL '/students/{id}', donde {id} es un parametro de la ruta por
 por ejemplo '/students/1 La funcion devuelve solo un estudiante */
 
-Route::post('/students', function (){
-    return 'Creando Estudiante';
-});
+Route::post('/students', [studentController::class, 'store']);
 /*3°POST/student: Esta ruta responde a solicitudes POST a la URL /students tipicamente utilizada para crear nuevos recursos en este caso 
 un estudiante. La funcion devuelve Creando estudiantes */
 
-Route::put('/students/{id}', function (){
-    return 'Actualizando estudiante';
-});
-/*4° */
-Route::delete('/students', function(){
-    return 'Eliminando estudiante';
-});
+Route::put('/students/{id}', [studentController::class, 'actualizar']);
+/*4° PUT/students/: Esta ruta responde a solicitudes PUT a la URL /students/{id} y se utiliza para actualizar un recurso existente
+estudiante. La funcion devuelve Actualizando estudiante */
+
+Route::patch('/students/{id}', [studentController::class, 'actualizacionParcial']);
+
+Route::delete('/students/{id}', [studentController::class, 'borrar']);
+/*5° DELETE/students/{id}: Esta ruta responde a solicitudes DELETE a la URL /students/{id}, utilizada para eliminar un recurso existente 
+(Estudiante).La funcion Devuelve Eliminando estudiante */
+?>
